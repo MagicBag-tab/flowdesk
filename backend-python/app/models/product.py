686e399860596ced6dc9760339caecae9a340f44
Tiffany SalSua <177962291+Tiffany24630@users.noreply.github.com>
2026-04-08 08:05:28 -0600

    Actualización en routes y modificaciones en product

--- backend-python/app/models/product.py
+++ backend-python/app/models/product.py
@@ -2,8 +2,12 @@ from sqlalchemy import Column, Integer, String, Float
 from app.db.database import Base
 
 class Product(Base):
-    _tablename_ = "products"
-    id = Column(Integer, primary_key = True, index = True)
-    name = Column(String, nullable = False)
-    price = Column(Float, nullable = False)
-    stock = Column(Integer, nullable = False)
+    _tablename_ = "producto"
+    id_producto = Column(Integer, primary_key = True, index = True)
+    id_proveedor = Column(Integer, ForeignKey("proveedor.id_proveedor"), nullable = True)
+    nombre = Column(String(100), nullable = False)
+    descripcion = Column(Text, nullable = True)
+    precio = Column(Numerico(10, 2), nullable = False)
+    stock_actual = Column(Integer, nullable = False, default = 0)
+    stock_minimo = Column(Integer, nullable = False, default = 0)
+    estado = Column(String(30), nullable = False)
---
+++ backend-python/app/routes/product_routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.controllers import product_controller
+from app.schemas.product_schema import ProductCreate, UpdateStock
+
+router = APIRouter()
+
+#Post products
+def create_product(product: ProductCreated, db: Session = Depends(get_db)):
+    return product_controller-create_product(db, product)
+
+#Get products
+def get_products(db: Session = Depends(get_db)):
+    return product_controller.get_products(db)
+
+#Patch products/id/stock
+def update_stock(product_id: int, data: UpdateStock, db: Session = Depends(get_db)):
+    return product_controller.update_stock(db, product_id, data)
